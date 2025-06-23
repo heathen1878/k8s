ffe760d6f83ac1a03abf7104bfb80dfd4d3fe99c
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-06-23 14:19:19 +0100

    updated script and kubeadm init

--- scripts/master.sh
+++ scripts/master.sh
@@ -118,6 +118,8 @@ echo "✅ Kubernetes ${K8S_VERSION} setup complete."
 JOIN_FILE="/vagrant/shared/join.sh"
 
 # 8. Initialize Kubernetes master node
+echo "Copy Service Account RSA Keys"
+cp /vagrant/manifests/sa.* /etc/kubernetes/pki/
 echo "🚀 Initializing Kubernetes master node...🔐 Using Federated Identity"
 kubeadm init --config="/vagrant/manifests/kubeadm-config.yml"
 
